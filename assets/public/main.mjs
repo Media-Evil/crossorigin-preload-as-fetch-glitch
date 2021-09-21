f8d810320e6dc10dba1eef4f85afd8b0f550055c
Media-Evil <47017377+Media-Evil@users.noreply.github.com>
2021-09-21 23:34:47 +0100

    Removed timeout to make the glitch clear

--- assets/public/main.mjs
+++ assets/public/main.mjs
@@ -1,18 +1,6 @@
 console.log('main initialized');
 
-// const response1 = fetch('https://localhost:9500/', {
-//   method: 'GET',
-//   mode: 'cors',
-//   credentials: 'include',
-//   //importance: 'high',
-//   headers: {
-//     'Cache-Control': 'max-age=16'
-//   }
-// }).then(response => response.json()).then(json => {
-//   console.log(json);
-// });
-
-setTimeout(() => {
+// setTimeout(() => { 
   const response2 = fetch('https://localhost:9500/', {
     method: 'GET',
     mode: 'cors',
@@ -24,4 +12,4 @@ setTimeout(() => {
   }).then(response => response.json()).then(json => {
     console.log(json);
   });
-}, 20);
+// }, 20);
